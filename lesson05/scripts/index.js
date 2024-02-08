3c91d9d999a9918b957194ba598bcc81f6d36c89
Takaedza <tmandie@live.com>
2024-02-09 01:07:40 +0200

    y

--- lesson05/scripts/index.js
+++ lesson05/scripts/index.js
@@ -1,4 +1,4 @@
-const input =document.querySelector('#favchap');
+/*const input =document.querySelector('#favchap');
 const button = document.querySelector('button');
 const list = document.querySelector('#list');
 
@@ -34,9 +34,37 @@ button.addEventListener('click', function(){
 
 
     
-});
+});*/
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
 
+button.addEventListener('click', () =>{
+    const myItem = input.value;
+    if (input.value !=''){
+        
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const deleteButton = document.createElement('button');
 
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(deleteButton);
+        deleteButton.textContent = '❌';
+        list.appendChild(listItem);
+
+        deleteButton.addEventListener('click', () => {
+          list.removeChild(listItem);
+        });
+        input.value =''
+        input.focus();
+    }
+
+    else{
+        window.alert('Can Please enter a Chapter!');
+        input.focus()
+    }
+})
  
 
 // copy right year
