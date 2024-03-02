fd2bb93abda2555fd338c213a567e5ee887e1183
Takaedza <tmandie@live.com>
2024-03-02 19:02:46 +0200

    formtest

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,41 @@
+//Password 
+const kp1 = document.querySelector("#pwd");
+const kp2 = document.querySelector("#pwd2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "❗PASSWORD DOES NOT MATCH!";
+		message.style.visibility = "show";
+		kp2.style.backgroundColor = "#fff0f3";
+		kp2.value = "";
+		kp2.focus();
+	}
+    
+    else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
+
+//range selection
+const rangevalue = document.getElementById("rangevalue");
+const range = document.getElementById("r");
+
+// RANGE event listener
+range.addEventListener('change', displayRatingValue);
+range.addEventListener('input', displayRatingValue);
+
+function displayRatingValue() {
+    rangevalue.innerHTML = range.value;
+}
+
+
+//page visitor number
 var counterContainer = document.querySelector(".page-visits");
 var visitCount = localStorage.getItem("page_view");
 
